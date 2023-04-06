b796f02119b739fc6b624ff598417f4c50800b0d
Martin Pihooja <martin.pihooja@gmail.com>
2023-04-06 20:09:14 +0300

    V1.9.1 Release - Bar active color bug fix

--- Elektrihind/BarChartView/BarChartCell.swift
+++ Elektrihind/BarChartView/BarChartCell.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 public struct BarChartCell : View {
+    @Environment(\.scenePhase) var scenePhase
     var value: Double
     var index: Int = 0
     var width: Float
@@ -18,17 +19,11 @@ public struct BarChartCell : View {
     }
     let currentHour = Calendar.current.component(.hour, from: Date())
     var accentColor: Color
-    var originalGradient: GradientColor?
-    var gradient: GradientColor? {
-        if day == .today {
-            return index == currentHour ? GradientColor(start: Color(hexString: "#FF964F"), end: Color(hexString: ":#FF964F")) : originalGradient
-        } else {
-            return originalGradient
-        }
-    }
-    
+    var originalGradient: GradientColor
+    @State var gradient: GradientColor?
     @State var scaleValue: Double = 0
     @Binding var touchLocation: CGFloat
+    
     public var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 4)
@@ -38,15 +33,29 @@ public struct BarChartCell : View {
             .scaleEffect(CGSize(width: 1, height: self.scaleValue), anchor: .bottom)
             .onAppear(){
                 self.scaleValue = self.value
+                self.gradient = getGradientColor()
+            }
+            .onChange(of: scenePhase) { newPhase in
+                if newPhase == .active {
+                    self.gradient = getGradientColor()
+                }
             }
         .animation(Animation.spring().delay(self.touchLocation < 0 ?  Double(self.index) * 0.04 : 0))
+    }
+    
+    func getGradientColor() -> GradientColor {
+        if day == .today {
+            return index == currentHour ? GradientColor(start: Color(hexString: "#FF964F"), end: Color(hexString: ":#FF964F")) : originalGradient
+        } else {
+            return originalGradient
+        }
     }
 }
 
 #if DEBUG
 struct ChartCell_Previews : PreviewProvider {
     static var previews: some View {
-        BarChartCell(value: Double(0.75), width: 320, numberOfDataPoints: 12, day: .today, accentColor: Colors.OrangeStart, originalGradient: nil, touchLocation: .constant(-1))
+        BarChartCell(value: Double(0.75), width: 320, numberOfDataPoints: 12, day: .today, accentColor: Colors.OrangeStart, originalGradient: GradientColor(start: Color.red, end: Color.blue), touchLocation: .constant(-1))
     }
 }
 #endif
