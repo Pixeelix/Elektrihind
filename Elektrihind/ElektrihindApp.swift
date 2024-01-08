8cf4c6b067ef95e18f880fb7a300cb8562341dba
Martin Pihooja <martin.pihooja@gmail.com>
2024-01-09 00:06:20 +0200

    V2.8.1 ATT & UMP fix

--- Elektrihind/ElektrihindApp.swift
+++ Elektrihind/ElektrihindApp.swift
@@ -15,24 +15,45 @@ import FBAudienceNetwork
 struct ElektrihindApp: App {
     @StateObject var networkManager = NetworkManager()
     @StateObject var shared = Globals()
-    init() {
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
-        FirebaseApp.configure()
-        FBAdSettings.setAdvertiserTrackingEnabled(true)
-    }
+    @State var adStatus: Int = 0
+    
     var body: some Scene {
         WindowGroup {
             ZStack {
-                ContentView()
-                    .environmentObject(networkManager)
-                    .environmentObject(shared)
-                UMPWrapper(canLoadAdsCallback: {
-                    debugPrint("Can load ads now")
-                })
-                .disabled(true)
+                switch adStatus {
+                case 0:
+                    HStack {}
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .top)
+                    .background(Color.backgroundColor.edgesIgnoringSafeArea(.all))
+                case 1:
+                    ContentView()
+                        .environmentObject(networkManager)
+                        .environmentObject(shared)
+                    UMPWrapper(canLoadAdsCallback: {
+                        debugPrint("Can load ads now")
+                    })
+                    .disabled(true)
+                case 2:
+                    ContentView()
+                        .environmentObject(networkManager)
+                        .environmentObject(shared)
+                default:
+                    ContentView()
+                        .environmentObject(networkManager)
+                        .environmentObject(shared)
+                }
+            }.onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
+                ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
+                    switch status {
+                    case .authorized:
+                        adStatus = 1
+                    case .notDetermined, .restricted, .denied:
+                        adStatus = 2
+                    @unknown default:
+                        adStatus = 2
+                    }
+                    print("STATUS: \(status)") })
             }
         }
     }
 }
-
-
